bae2d090bff6e3d8a10cb0d300c7ff4328b96fdc
Arturo Salinas Ortiz <salor_arturo@hotmail.com>
2018-03-12 20:54:46 -0600

    First Commit
    This is the first commit for the Ionic lab 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,10 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+	form: any;
 
   constructor(public navCtrl: NavController) {
-
+  	this.form={};
   }
 
 }
